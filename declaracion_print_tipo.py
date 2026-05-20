115bf4c67308ed4e50aaafeb19c6988915a7526d
Adrian <saom.oliden@gmail.com>
2026-05-19 18:51:31 -0700

    CLASS 1
    This Python script marks the beginning of my programming journey. It covers the foundational basics of the language, including:
    
    * **Variable Declaration:** Storing different data types such as Strings (`str`) and Integers (`int`).
    * **Console Output:** Using the `print()` function to display text, multiple arguments, and variables.
    * **Data Type Inspection:** Utilizing the built-in `type()` function to identify and print the data type of variables, including floats (decimals).

---
+++ declaracion_print_tipo.py
@@ -0,0 +1,19 @@
+#HOLA SOLO SOY UN CHICO INTENTANDO APRENDER A PROGRAMAR 
+#DECLARACION DE VARIABLES
+nombre='Adrian Oliden'
+edad=19
+nacionalidad='Méxicano'
+
+#IMPRIMIR PALABRAS
+print('Hola Mundo')
+print('Mis colores favoritos son', 'negro', 'rojo', 'verde')
+
+#IMPRIMIR VARIABLES
+print('Mi nombre es:', nombre, ' mi edad es:', edad, ' y soy:', nacionalidad)
+
+#COMO SABER EL TIPO DE UNA VARIABLE
+print(nombre, type(nombre))
+print(edad, type(edad))
+print(nacionalidad, type(nacionalidad))
+pi=3.1416
+print(pi, type(pi))
